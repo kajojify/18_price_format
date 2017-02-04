44652dffd33d542eb2a2742fc294681db0ba4c92
Samoilenko Roman <rsamoilenko@mail.ru>
2017-02-04 21:46:04 +0200

    Update format_price.py

--- format_price.py
+++ format_price.py
@@ -1,5 +1,23 @@
-def format_price(price):
-    pass
+import argparse
+
+
+def get_formatted_price(price_string):
+    price = float(price_string.replace(',', '.'))
+    if price.is_integer():
+        price_formatted = "{:,.0f}".format(price)
+    else:
+        price_formatted = "{:,.2f}".format(price)
+    return price_formatted.replace(',', ' ')
+
 
 if __name__ == '__main__':
-    pass
+    parser = argparse.ArgumentParser(description="Price formatter")
+    parser.add_argument("price_string", help="the price you want to format")
+    args = parser.parse_args()
+    try:
+        pretty_price = get_formatted_price(args.price_string)
+    except ValueError as error:
+        print("Something went wrong!", error)
+        exit("Exiting...")
+    print(pretty_price)
+    
